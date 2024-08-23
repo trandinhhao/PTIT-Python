a3e4e667c90d1c2edce9f60868f3ac42d4d5cde2
Trần Đình Hào <121801645+trandinhhao@users.noreply.github.com>
2024-08-23 20:02:28 +0700

    Add files via upload

---
+++ PY02019.py
@@ -0,0 +1,12 @@
+
+from math import *
+from sys import *
+
+if __name__ == '__main__':
+    t = int(input())
+    a = [int(x) for x in input().split()]
+    a.sort()
+    for i in range(len(a)):
+        for j in range(i+1,len(a)):
+            if gcd(a[i],a[j]) == 1:
+                print(a[i], a[j])
