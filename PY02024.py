06c5ba1860ad53659cf0ad09c5970a411608dc2f
Trần Đình Hào <121801645+trandinhhao@users.noreply.github.com>
2024-08-24 00:24:43 +0700

    Add files via upload

---
+++ PY02024.py
@@ -0,0 +1,18 @@
+
+from math import *
+from sys import *
+
+def sxep(n):
+    tmp = n
+    res = 1
+    while n>0:
+        res *= n%10
+        n//=10
+    return (res,tmp)
+
+if __name__ == '__main__':
+    for test in range(int(input())):
+        n = int(input())
+        a = [int(x) for x in input().split()]
+        a.sort(key= sxep)
+        print(*a)
