f7486f1f4631beea5ef7d35a38bce02ee20c7038
Trần Đình Hào <121801645+trandinhhao@users.noreply.github.com>
2024-12-26 14:46:14 +0700

    Add files via upload

---
+++ PY02056.py
@@ -0,0 +1,24 @@
+
+def check(s):
+    if len (s) == 1: return False
+    if int(s) == int(s[::-1]):
+        return True
+    else:
+        return False
+
+n, m = map(int, input().split())
+a = []
+for _ in range(n):
+    a.append([int(x) for x in input().split()])
+save = -1
+for i in range (n):
+    for j in range(m):
+        if check(str(a[i][j])) and a[i][j] > save:
+            save = a[i][j]
+if save == -1: print('NOT FOUND')
+else:
+    print(save)
+    for i in range(n):
+        for j in range(m):
+            if a[i][j] == save:
+                print('Vi tri [%s][%s]' % (i,j))
